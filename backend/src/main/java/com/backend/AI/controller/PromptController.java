938b9903608cf0736d8c618c69a1ceed9a744d0f
savaliyabhargav <savaliyabhargav644@gmail.com>
2025-08-30 16:37:48 +0530

    geting prompt from forntend and save that on backend in prompt.txt file

--- backend/src/main/java/com/backend/AI/controller/PromptController.java
+++ backend/src/main/java/com/backend/AI/controller/PromptController.java
@@ -1,14 +1,22 @@
 package com.backend.AI.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 @RestController
+@RequestMapping("/api")
 public class PromptController {
 
-    @GetMapping("/api/hello")
-    public String sayHello() {
-        return "Hello from Backend!";
+    @PostMapping("/save")
+    public String savePrompt(@RequestBody String prompt) {
+        try (FileWriter writer = new FileWriter("prompt.txt",true)){
+            writer.write(prompt + System.lineSeparator());
+            return "Prompt saved successfully!";
+        }catch (IOException e){
+            return "error saving prompt : " + e.getMessage();
+        }
     }
 }
 
